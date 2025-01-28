94ae386d65bc49cf3cad55c3fc42b09df0212c89
Jonas Hoelscher <jonasho@mail.uni-paderborn.de>
2025-01-28 18:06:33 +0100

    Update coloscheme

--- init.lua
+++ init.lua
@@ -154,7 +154,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+-- vim.opt.scrolloff = 10
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -860,18 +860,21 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'sainnhe/sonokai',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'sonokai'
-      vim.g.sonokai_enable_italic = true
-      vim.g.sonokai_style = 'shusia'
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
+    -- 'sainnhe/sonokai',
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    -- vim.cmd.colorscheme 'sonokai'
+    -- vim.g.sonokai_enable_italic = true
+    -- vim.g.sonokai_style = 'shusia'
+    -- You can configure highlights by doing something like:
+    -- vim.cmd.hi 'Comment gui=none'
+    -- end,
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = true,
   },
 
   {
@@ -989,7 +992,7 @@ require('lazy').setup({
   },
 })
 
-vim.cmd 'colorscheme sonokai'
+vim.cmd 'colorscheme gruvbox'
 vim.o.textwidth = 79
 vim.o.colorcolumn = '79'
 vim.g.vimtex_compiler_latexmk = {
